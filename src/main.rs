63286f5d1543eb7610a1c1e8b628bef64317cf65
Asko Nõmm <asko@repl.ee>
2023-10-19 18:18:26 +0300

    Implement more DSL options.

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,28 @@ struct ConfigDataDSLWhenIs {
 }
 
 #[derive(Deserialize, Debug)]
+struct ConfigDataDSLWhenIsNot {
+    key: String,
+    equals: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ConfigDataDSLWhenHas {
+    key: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ConfigDataDSLWhenHasNot {
+    key: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ConfigDataDSLWhenMatches {
+    key: String,
+    regex: String,
+}
+
+#[derive(Deserialize, Debug)]
 struct ConfigDataDSLSort {
     key: String,
     order: String,
@@ -50,6 +72,10 @@ struct ConfigDataDSL {
     name: String,
     collection: String,
     when_is: Option<ConfigDataDSLWhenIs>,
+    when_is_not: Option<ConfigDataDSLWhenIsNot>,
+    when_has: Option<ConfigDataDSLWhenHas>,
+    when_has_not: Option<ConfigDataDSLWhenHasNot>,
+    when_matches: Option<ConfigDataDSLWhenMatches>,
     sort: Option<ConfigDataDSLSort>,
     limit: Option<usize>,
 }
@@ -102,6 +128,30 @@ fn compose_tera_context() -> Result<Context, Error> {
                 records = records.when_is(&when_is.key, &when_is.equals);
             }
 
+            // when_is_not
+            if data.when_is_not.is_some() {
+                let when_isnt = data.when_is_not.unwrap();
+                records = records.when_isnt(&when_isnt.key, &when_isnt.equals);
+            }
+
+            // when_has
+            if data.when_has.is_some() {
+                let when_has = data.when_has.unwrap();
+                records = records.when_has(&when_has.key);
+            }
+
+            // when_has_not
+            if data.when_has_not.is_some() {
+                let when_has_not = data.when_has_not.unwrap();
+                records = records.when_hasnt(&when_has_not.key);
+            }
+
+            // when_matches
+            if data.when_matches.is_some() {
+                let when_matches = data.when_matches.unwrap();
+                records = records.when_matches(&when_matches.key, &when_matches.regex);
+            }
+
             // sort
             if data.sort.is_some() {
                 let sort = data.sort.unwrap();
@@ -126,6 +176,7 @@ fn compose_tera_context() -> Result<Context, Error> {
     Ok(context)
 }
 
+// Helper function to get everything before a character
 fn str_before_char(input: &str, char: &str) -> String {
     let parts: Vec<&str> = input.split(char).collect();
 
